7d119af05fbef9e7557e15338c4dba4036aeb6b0
Maryna <tromsa3122@mail.com>
2025-01-17 11:31:25 +0200

    add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,27 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit',onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  sendUserInfo(e);
+}
+
+const sendUserInfo = (e) => {
+  const form = e.target;
+  const login = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (!login || !password) {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    email: login,
+    password: password,
+  };
+  console.log(formData);
+
+  formLogin.reset();
+};
